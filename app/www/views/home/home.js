c38bf7ba099d64bb635ce7587e0b869eea64bafb
Sabino Velasquez <sabinovelasquez@gmail.com>
2016-10-05 16:59:24 -0300

    selección de departamentos

--- app/www/views/home/home.js
+++ app/www/views/home/home.js
@@ -5,13 +5,18 @@
         .module('WeBarrio.controllers')
         .controller('homeController', homeController);
 
-    function homeController($scope, $state, dataAPIService) {
+    function homeController($scope, $state, dataAPIService, $ionicSlideBoxDelegate) {
       $scope.home = {};
       $scope.user = {};
       dataAPIService.getUser().then( function(userData){
         $scope.user = userData.data.user;
         $scope.home.condos = userData.data.condos;
+        $scope.currentDepto = $scope.home.condos[0].departments;
+        $ionicSlideBoxDelegate.update();
       }); 
+      $scope.slideHasChanged = function($index){
+        $scope.currentDepto = $scope.home.condos[$index].departments;
+      };
     }
 
 }).call(this);
