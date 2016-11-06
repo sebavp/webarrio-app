d771098cde675d1143a930f754c7723e1a0358c2
Sebastián Schuchhardt <seba.sch999@gmail.com>
2016-11-06 14:25:05 -0300

    fix url api

--- www/js/services/utils.js
+++ www/js/services/utils.js
@@ -2,32 +2,53 @@ angular.module('WeBarrio.utils', []).factory('Utils', function($ionicLoading,$io
 
 	var Utils = {
 
-	    show: function() {
-	      $ionicLoading.show({
-	  	    animation: 'fade-in',
-	  	    showBackdrop: false,
-	  	    maxWidth: 200,
-	  	    showDelay: 500,
-	        template: 'Cargando...'
-	      });
-	    },
+    show: function() {
+      $ionicLoading.show({
+  	    animation: 'fade-in',
+  	    showBackdrop: false,
+  	    maxWidth: 200,
+  	    showDelay: 500,
+        template: 'Cargando...'
+      });
+    },
 
-	    hide: function(){
-	      $ionicLoading.hide();
-	    },
+    hide: function(){
+      $ionicLoading.hide();
+    },
 
-		alertshow: function(tit,msg){
-			var alertPopup = $ionicPopup.alert({
-				title: tit,
-				template: msg
-			});
-			alertPopup.then(function(res) {
-				//console.log('Registrado correctamente.');
-			});
-		},
+	alertshow: function(tit,msg){
+		var alertPopup = $ionicPopup.alert({
+			title: tit,
+			template: msg
+		});
+		alertPopup.then(function(res) {
+			//console.log('Registrado correctamente.');
+		});
+	},
+
+	errMessage: function(err) {
+
+	    var msg = "Ha ocurrido un error...";
+
+	    if(err && err.code) {
+	      switch (err.code) {
+	        case "EMAIL_TAKEN":
+	          msg = "This Email has been taken."; break;
+	        case "INVALID_EMAIL":
+	          msg = "Invalid Email."; break;
+	      case "NETWORK_ERROR":
+	          msg = "Network Error."; break;
+	        case "INVALID_PASSWORD":
+	          msg = "Invalid Password."; break;
+	        case "INVALID_USER":
+	          msg = "Invalid User."; break;
+	      }
+	    }
+			Utils.alertshow("Error",msg);
+	},
 
 
-	  };
+  };
 
 	return Utils;
 
